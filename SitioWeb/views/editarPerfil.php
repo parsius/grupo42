58a994e920ba2b0facb4cce275c6b95ac97a9c45
parsius <pablo25.1996@hotmail,com>
2018-06-25 16:39:41 -0300

    moddif file
    editar perfil puede cambier el nombre de usuario

--- SitioWeb/views/editarPerfil.php
+++ SitioWeb/views/editarPerfil.php
@@ -4,8 +4,34 @@
 	require '../functions.php';
 	$errores = '';
 //	$usuario=$_SESSION['usuario'];
-	
-		$dominio = $_POST['dominio'];
+	   $dominio = $_POST['dominio'];
+//BUSCO SI EL NOMBRE EXISTE
+        $id = $_POST['usuario'];
+        $con = mysqli_connect("localhost", "root", "", "aventon") or die ("no se pudo conectar");
+        mysqli_select_db($con, 'aventon');
+        $query = "SELECT usuario FROM usuario WHERE usuario = '$id'";
+        $resultado = mysqli_query($con, $query);
+        $totalFilas    =    mysqli_num_rows($resultado); 
+        if ($totalFilas == 0){  
+        //MODIFICO EL NOMBRE EN LAS TABLAS
+         $conexion=conexion();
+         $statemen = $conexion->prepare('UPDATE usuario SET usuario = :id WHERE usuario = :usuario ');
+		 $statemen->execute(array(':id' => $id,
+                                 ':usuario' => $dominio));   
+         $statemen = $conexion->prepare('UPDATE vehiculos SET usuario3 = :id WHERE usuario3 = :usuario ');
+		 $statemen->execute(array(':id' => $id,
+                                 ':usuario' => $dominio));      
+         $statemen = $conexion->prepare('UPDATE viajes SET usuario2 = :id WHERE usuario2 = :usuario ');
+		 $statemen->execute(array(':id' => $id,
+                                 ':usuario' => $dominio));
+          $statemen = $conexion->prepare('UPDATE postulantes SET idpiloto = :id WHERE idpiloto = :usuario ');
+		  $statemen->execute(array(':id' => $id,
+                                 ':usuario' => $dominio));
+           $statemen = $conexion->prepare('UPDATE mensajes SET idusuario = :id WHERE idusuario = :usuario ');
+		   $statemen->execute(array(':id' => $id,
+                                 ':usuario' => $dominio));  
+        //MODIFICO EL RESTO DE VARIABLES
+		
 	//	$dominio = $_POST['id'];
 		$capacidad = $_POST['capacidad'];
 		$tipo = $_POST['tipo'];
@@ -23,7 +49,7 @@
 	//dominio= usuario, capacidad=nombre, tipo= apellido, modelo= email
 
 	require '../views/editarView.php';
-
+        }else{header('Location: ' . RUTA . '/index.php');}
 
 
 ?>
